b99457efc85c0635a68c4245989eef02767cd793
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-04-13 23:29:03 +0300

    chore: add initial project structure with HTML, CSS, and server setup

---
+++ app/public/scripts.js
@@ -0,0 +1,8 @@
+const socket = io('http://localhost:9000');
+socket.on('connect', () => {
+    console.log('Connected!');
+    socket.emit('clientConnect');
+})
+socket.on('welcome', (data) => {
+    console.log(data);
+})
---
+++ app/slack.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io');
+
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9000);
+const io = socketio(expressServer)
+
+io.on('connection', (socket) => {
+    socket.emit('welcome', 'Welcome to the server!');
+    socket.on('clientConnect', (data) => {
+        console.log(socket.id, 'has connected');
+    })
+})
+
+
+
