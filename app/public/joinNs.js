a8dabaf692b3a9bec487be0f6e624a712fb5b9f4
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-04-14 21:47:59 +0300

    feat: implement room joining functionality and add message building logic

--- app/public/joinNs.js
+++ app/public/joinNs.js
@@ -1,11 +1,35 @@
-const joinNs = (elem, nsData) =>{
-    const nsEndpoint = elem.getAttribute('ns');
-    const clickedNs = nsData.find(row => row.endpoint === nsEndpoint);
+const joinNs = (element,nsData)=>{
+    const nsEndpoint = element.getAttribute('ns');
+    console.log(nsEndpoint);
+
+    const clickedNs = nsData.find(row=>row.endpoint === nsEndpoint);
+    selectedNsId = clickedNs.id;
     const rooms = clickedNs.rooms;
+
     let roomList = document.querySelector('.room-list');
-    roomList.innerHTML = '';
-    rooms.forEach(room => {
-        roomList.innerHTML += `<li><span class="glyphicon glyphicon-lock"></span>${room.roomTitle}</li>`
+    roomList.innerHTML = "";
+
+    let firstRoom;
+
+    rooms.forEach((room,i)=>{
+        if(i === 0){
+            firstRoom = room.roomTitle;
+        }
+        console.log(room);
+        roomList.innerHTML += `<li class="room" namespaceId=${room.namespaceId}>
+            <span class="fa-solid fa-${room.privateRoom ? 'lock' : 'globe'}"></span>${room.roomTitle}
+        </li>`
     })
-    localStorage.setItem('lastNs', nsEndpoint);
+
+    joinRoom(firstRoom,clickedNs.id)
+
+    const roomNodes = document.querySelectorAll('.room');
+    Array.from(roomNodes).forEach(elem=>{
+        elem.addEventListener('click',e=>{
+            const namespaceId = elem.getAttribute('namespaceId')
+            joinRoom(e.target.innerText,namespaceId)
+        })
+    })
+
+    localStorage.setItem('lastNs',nsEndpoint);
 }
---
+++ app/public/joinRoom.js
@@ -0,0 +1,14 @@
+const joinRoom = async(roomTitle,namespaceId)=>{
+
+    console.log(roomTitle,namespaceId);
+    const ackResp = await nameSpaceSockets[namespaceId].emitWithAck('joinRoom',{roomTitle,namespaceId});
+    console.log(ackResp);
+    document.querySelector('.curr-room-num-users').innerHTML = `${ackResp.numUsers}<span class="fa-solid fa-user"></span>`
+    document.querySelector('.curr-room-text').innerHTML = roomTitle;
+
+    document.querySelector('#messages').innerHTML = "";
+
+    ackResp.thisRoomsHistory.forEach(message=>{
+        document.querySelector('#messages').innerHTML += buildMessageHtml(message)
+    })
+}
